6b52bfa90471177f07f3884028bcbcdbb91abbcd
puriakazemieh <puriakazemieh@gmail.com>
2024-12-03 16:57:12 +0330

    [STYLE] Create MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,8 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
 }
